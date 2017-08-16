5b0981ca259a7cfa09f3f5c9a524d72679d66cb7
pavel-shilyagov <shilyagov.p@gmail.com>
2017-08-17 00:55:14 +0300

    Few more tests for DU

--- LiteDB.FSharp.Tests/Mapper.fs
+++ LiteDB.FSharp.Tests/Mapper.fs
@@ -4,17 +4,38 @@ open Expecto
 
 type Shape =
   | Circle of r:float
-  | Square of w:float*h:float
+  | Rectangle of w:float*h:float
+  | Point
 
 [<Tests>]
 let tests =
-  testList "Mapper" [
-    testCase "DU" <| fun _ ->
+  testList "Shapes" [
+    testCase "Circle" <| fun _ ->
       let shape = Circle(4.0)
       let mapper = LiteDB.FSharp.Mapper.instance
       let doc = mapper.ToDocument shape
       let no = mapper.ToObject<Shape>(doc)
       match no with
       | Circle r -> Expect.floatClose Accuracy.low r 4.0 "radius"
-      | Square (_) -> failtest "Should be Circle"
+      | _ -> failtest "Should be Circle"
+
+    testCase "Square" <| fun _ ->
+      let shape = Rectangle(2.0,3.0)
+      let mapper = LiteDB.FSharp.Mapper.instance
+      let doc = mapper.ToDocument shape
+      let no = mapper.ToObject<Shape>(doc)
+      match no with
+      | Rectangle (w,h) ->
+          Expect.floatClose Accuracy.low w 2.0 "width"
+          Expect.floatClose Accuracy.low h 3.0 "height"
+      | _ -> failtest "Should be Rectangle"
+
+    testCase "Point" <| fun _ ->
+      let shape = Point
+      let mapper = LiteDB.FSharp.Mapper.instance
+      let doc = mapper.ToDocument shape
+      let no = mapper.ToObject<Shape>(doc)
+      match no with
+      | Point -> Expect.isTrue true ""
+      | _ -> failtest "Should be Circle"
   ]
