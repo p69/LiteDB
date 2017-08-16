0eea2d4d49af854ed0288476e97c59c8e7a6b7a8
pavel-shilyagov <pshilyagov@viber.com>
2017-08-16 16:36:27 +0300

    Function stubs for DU (de)serialization

--- LiteDB.FSharp/BsonMapper.fs
+++ LiteDB.FSharp/BsonMapper.fs
@@ -1,8 +1,31 @@
 namespace LiteDB.FSharp
 open LiteDB
 open System
+open FSharp.Reflection
 
 module Mapper =
+    let private serialize t entity : BsonDocument =
+        BsonDocument()
+
+    let private deserialize t doc : obj =
+        obj()
+
     let instance = { new BsonMapper(customTypeInstantiator=null) with
-        override x.ToObject (typeInfo:Type, doc:BsonDocument) = "" :> obj
+        override x.ToDocument (typeInfo:Type, entity:obj) =
+            if (isNull entity) then raise <| ArgumentNullException "entity"
+            if (entity :? BsonDocument)
+            then entity :?> BsonDocument
+            else
+                match FSharpType.IsUnion typeInfo with
+                | true -> serialize typeInfo entity
+                | false -> x.Serialize(typeInfo, entity, 0).AsDocument
+        override x.ToObject (typeInfo:Type, doc:BsonDocument) =
+            if (isNull doc) then raise <| ArgumentNullException "doc"
+            if (typeInfo = typeof<BsonDocument>)
+            then doc :> obj
+            else
+                match FSharpType.IsUnion typeInfo with
+                | true -> deserialize typeInfo doc
+                | false -> x.Deserialize(typeInfo, doc)
+
       }
