d4279f12d1b18db368d22c88d843574752bf999b
pavel-shilyagov <pshilyagov@viber.com>
2017-08-18 17:29:50 +0300

    Fix DU with DU as fields. Added more tests.

--- LiteDB.FSharp.Tests/Mapper.fs
+++ LiteDB.FSharp.Tests/Mapper.fs
@@ -7,6 +7,12 @@ type Shape =
   | Rectangle of w:float*h:float
   | Point
 
+type Single = A
+
+type CompositeDU =
+  | CA of s:Shape*name:string
+  | CB of Single
+
 [<Tests>]
 let tests =
   testList "Shapes" [
@@ -37,5 +43,25 @@ let tests =
       let no = mapper.ToObject<Shape>(doc)
       match no with
       | Point -> Expect.isTrue true ""
-      | _ -> failtest "Should be Circle"
+      | _ -> failtest "Should be Point"
+
+    testCase "Single" <| fun _ ->
+      let mapper = LiteDB.FSharp.Mapper.instance
+      let doc = mapper.ToDocument A
+      let no = mapper.ToObject<Single>(doc)
+      match no with
+      | A -> Expect.isTrue true ""
+
+    testCase "Composite" <| fun _ ->
+      let mapper = LiteDB.FSharp.Mapper.instance
+      let ca = CA(Shape.Circle(2.0), "circle")
+      let doc = mapper.ToDocument ca
+      let no = mapper.ToObject<CompositeDU>(doc)
+      match no with
+      | CA (s,n) ->
+          Expect.equal n "circle" "name"
+          match s with
+          | Circle r -> Expect.floatClose Accuracy.low r 2.0 "radius"
+          | _ -> failtest "Should be Circle"
+      | _ -> failtest "Should be CA"
   ]
--- LiteDB.FSharp/BsonMapper.fs
+++ LiteDB.FSharp/BsonMapper.fs
@@ -2,53 +2,72 @@ namespace LiteDB.FSharp
 open LiteDB
 open System
 open FSharp.Reflection
+open System.Reflection
 
 module Mapper =
-    let private toString (x:'a) =
-        match FSharpValue.GetUnionFields(x, typeof<'a>) with
-        | case, _ -> case.Name
-
     let private fromString (t:Type) (s:string) (fields:obj array) =
         match FSharpType.GetUnionCases t |> Array.filter (fun case -> case.Name = s) with
         |[|case|] -> Some(FSharpValue.MakeUnion(case,fields) :?> 'a)
         |_ -> None
 
+    let serializeUnion (typeInfo:Type) (o:obj) (depth:int) (mapper:BsonMapper) =
+        let doc = BsonDocument()
+        let (case, fields) = FSharpValue.GetUnionFields(o, typeInfo)
+        doc.RawValue.["_du"] <- BsonValue(case.Name)
+        doc.RawValue.["_type"] <- BsonValue(typeInfo.FullName + ", " + typeInfo.GetTypeInfo().Assembly.GetName().Name)
+        if (fields.Length > 0)
+        then
+            let bsonArray = BsonArray(Array.empty<BsonValue>)
+            fields
+            |> Array.iter (fun i -> bsonArray.Add(mapper.Serialize(i.GetType(), i, depth+1)))
+            |> ignore
+            doc.RawValue.["_duf"] <- bsonArray
+        doc :> BsonValue
+
+    let desirializeUnion (typeInfo:Type) (value:BsonValue) (mapper:BsonMapper) =
+        let doc = value.AsDocument
+        match doc.TryGetValue "_du" with
+        | (true, s) ->
+            let bsonFields =
+                match doc.TryGetValue "_duf" with
+                | (true, f) -> f.AsArray
+                | (false, _) -> BsonArray(Array.empty<BsonValue>)
+            let fields =
+                bsonFields
+                |> Seq.map (fun i ->
+                        let mutable t = typeof<obj>
+                        if (i.IsDocument)
+                        then
+                            match i.AsDocument.TryGetValue "_type" with
+                            | (true, typeName) -> t<-Type.GetType(typeName.AsString)
+                            | (false, _) ->()
+                        mapper.Deserialize(t, i)
+                    )
+                |> Seq.toArray
+            let duOption = fromString typeInfo s.AsString fields
+            match duOption with
+            | Some du -> du
+            | None -> raise <| InvalidOperationException "Couldn't create DU instance. Probably serialization wasn't made by LiteDB.FSharp.BsonMapper"
+        | (false, _) -> raise <| InvalidOperationException "Couldn't find '_du' prefix. Probably serialization wasn't made by LiteDB.FSharp.BsonMapper"
+
+
     let instance = { new BsonMapper(customTypeInstantiator=null) with
         override x.Serialize(typeInfo:Type, o:obj, depth:int) =
             if (isNull o) then BsonValue.Null
             else
-                match FSharpType.IsUnion typeInfo with
-                | true ->
-                    let doc = BsonDocument()
-                    let (case, fields) = FSharpValue.GetUnionFields(o, typeInfo)
-                    doc.RawValue.["_du"] <- BsonValue(case.Name)
-                    if (fields.Length > 0)
-                    then
-                        doc.RawValue.["_duf"] <- x.Serialize(typeof<obj array>, fields, depth+1)
-                    doc :> BsonValue
-                | false -> base.Serialize(typeInfo, o, depth)
+            match FSharpType.IsUnion typeInfo with
+            | true -> serializeUnion typeInfo o depth x
+            | false -> base.Serialize(typeInfo, o, depth)
 
         override x.Deserialize(typeInfo:Type, value:BsonValue) =
             if (isNull value) then null else
             if (typeInfo = typeof<BsonValue>)
             then value :> obj
             else
-                match FSharpType.IsUnion typeInfo with
-                | true ->
-                    let doc = value.AsDocument
-                    match doc.TryGetValue "_du" with
-                    | (true, s) ->
-                        let bsonFields =
-                            match doc.TryGetValue "_duf" with
-                            | (true, f) -> f.AsArray
-                            | (false, _) -> BsonArray(Array.empty<BsonValue>)
-                        let fields = x.DeserializeArray(typeof<obj>, bsonFields)
-                        let ff = fields |> Seq.cast<obj> |> Seq.toArray
-                        let duOption = fromString typeInfo s.AsString ff
-                        match duOption with
-                        | Some du -> du
-                        | None -> raise <| InvalidOperationException "Couldn't create DU instance. Probably serialization wasn't made by LiteDB.FSharp.BsonMapper"
-                    | (false, _) -> raise <| InvalidOperationException "Couldn't find '_du' prefix. Probably serialization wasn't made by LiteDB.FSharp.BsonMapper"
-                | false -> base.Deserialize(typeInfo, value)
-
+            match FSharpType.IsUnion typeInfo with
+            | true -> desirializeUnion typeInfo value x
+            | false ->
+                if (value.IsDocument && value.AsDocument.ContainsKey("_du"))
+                then desirializeUnion typeInfo value x
+                else base.Deserialize(typeInfo, value)
       }
